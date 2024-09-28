0479eb24cc29daccc6c4ac412fc9e6b9b0845e1d
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-09-28 12:00:31 +0300

    feat: implement getFalsyValuesCount function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -363,8 +363,15 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((count, value) => (value === false
+      || value === null
+      || value === 0
+      || value === ''
+      || value === undefined
+      || Number.isNaN(value)
+    ? count + 1
+    : count), 0);
 }
 
 /**
