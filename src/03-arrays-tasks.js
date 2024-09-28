c14f264073253b35575c0c767d981c500bb0ada1
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-09-28 11:43:01 +0300

    feat: implement getMovingSum function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -232,8 +232,11 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.map((el, index, array) => array.slice(0, index + 1).reduce(
+    (acc, current) => acc + current,
+    0,
+  ));
 }
 
 /**
