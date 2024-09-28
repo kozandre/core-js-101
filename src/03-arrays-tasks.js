2b6dd9715766d0dba106e39d6e0b69fede75120f
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-09-28 23:27:30 +0300

    feat: implement swapHeadAndTail function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -592,8 +592,15 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const head = arr.slice(0, middle);
+  const tail = arr.slice(-middle);
+  const middleElement = arr.length % 2 === 0 ? [] : [arr[middle]];
+  return tail.concat(middleElement, head);
 }
 
 module.exports = {
